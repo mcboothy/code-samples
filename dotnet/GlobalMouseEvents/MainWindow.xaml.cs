e3d73d2aad58bff2fde7e4c3266807761bb70550
Russ Booth <mcboothy@hotmail.co.uk>
2025-09-04 19:45:43 +0100

    Using RegisterRawInputDevices for global mouse move

---
+++ dotnet/GlobalMouseEvents/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿namespace GlobalMouseEvents;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using static GlobalMouseEvents.NativeHelper;
+
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private HwndSource? _source;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        _source = (HwndSource)PresentationSource.FromVisual(this);
+        _source.AddHook(WndProc);
+
+        RAWINPUTDEVICE input = new() 
+        {
+            usUsagePage = 0x01,
+            usUsage = 0x02, // Mouse
+            dwFlags = RIDEV_INPUTSINK,
+            hwndTarget = _source.Handle
+        };
+
+        if (!RegisterRawInputDevices(ref input, 1, (uint)Marshal.SizeOf<RAWINPUTDEVICE>()))
+        {
+            MessageBox.Show($"Stuff went wrong RegisterRawInputDevices: Error Code : {Marshal.GetLastWin32Error()}");
+        }
+    }
+
+    private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        switch (msg)
+        {
+            case WM_INPUT:
+                {
+                    // call GetRawInputData to get the size of the data RAWINPUTHEADER
+                    uint dwSize = 0;
+                    GetRawInputData(lParam, RID_INPUT, IntPtr.Zero, ref dwSize, (uint)Marshal.SizeOf<RAWINPUTHEADER>());
+                    IntPtr buffer = Marshal.AllocHGlobal((int)dwSize);
+                    try
+                    {
+                        // call it again to return the buffer
+                        if (GetRawInputData(lParam, RID_INPUT, buffer, ref dwSize, (uint)Marshal.SizeOf<RAWINPUTHEADER>()) == dwSize)
+                        {
+                            RAWINPUT raw = Marshal.PtrToStructure<RAWINPUT>(buffer);
+                            if (raw.header.dwType == RM_TYPEMOUSE)
+                            {
+                                OnGlobalMouseMove(raw.mouse.lLastX, raw.mouse.lLastY);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        Marshal.FreeHGlobal(buffer);
+                    }
+
+                    handled = false;
+                }
+                break;
+        }
+
+        return IntPtr.Zero;
+    }
+
+    private void OnGlobalMouseMove(int dx, int dy)
+    {
+        Debug.WriteLine($"Mouse Moved : x = {dx}, y = {dy}");
+    }
+}
---
+++ dotnet/GlobalMouseEvents/NativeHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalMouseEvents
+{
+    public static class NativeHelper
+    {
+        public const int WM_INPUT = 0x00FF;
+        public const int RIDEV_INPUTSINK = 0x00000100;
+        public const int RID_INPUT = 0x10000003;
+
+        public const int RM_TYPEMOUSE = 0;
+        public const int RM_TYPEKEYBOARD = 1;
+        public const int RM_TYPEHID = 2;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTDEVICE
+        {
+            public ushort usUsagePage;   
+            public ushort usUsage;     
+            public uint dwFlags;       
+            public IntPtr hwndTarget;  
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTHEADER
+        {
+            public uint dwType;          // 0 = mouse
+            public uint dwSize;
+            public IntPtr hDevice;
+            public IntPtr wParam;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWMOUSE
+        {
+            public ushort usFlags;
+            public uint ulButtons;
+            public ushort usButtonFlags;
+            public ushort usButtonData;
+            public uint ulRawButtons;
+            public int lLastX;           // relative movement
+            public int lLastY;
+            public uint ulExtraInformation;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUT
+        {
+            public RAWINPUTHEADER header;
+            public RAWMOUSE mouse;
+        }
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern bool RegisterRawInputDevices(
+            [In] ref RAWINPUTDEVICE pRawInputDevices,
+            uint uiNumDevices,
+            uint cbSize);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern uint GetRawInputData(
+            IntPtr hRawInput,
+            uint uiCommand,
+            IntPtr pData,
+            ref uint pcbSize,
+            uint cbSizeHeader);
+    }
+}
